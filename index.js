b49eb5da0cb550c611cc00d8d8da2ff45e535c59
shakibchakladar <shakibchakladar@gmail.com>
2024-06-09 01:51:20 +0600

    get data for advertisment

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const app =express();
+require('dotenv').config()
+const port =process.env.PORT ||5000;
+
+// middlewar
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9ttivus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const advertiesmentCollection=client.db('nextDwelldb').collection('advertisement')
+
+    app.get('/advertiesment',async(req,res)=>{
+        const result=await advertiesmentCollection.find().toArray();
+        res.send(result);
+
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req,res)=>{
+    res.send('nextdwell server is running')
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port : ${port}`)
+})
